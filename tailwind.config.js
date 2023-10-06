0bae5b79a0fdc295a64eebb76203d4664e780f4e
MuhamdFaj26 <muhammad.izzulhaq@students.amikom.ac.id>
2023-10-06 20:49:30 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily : {
+        "inter" : ["inter"],
+        "display" :["display"],
+        "CircularSTD" :["CircularSTD"]
+      },
+      colors : {
+
+      }
+    },
+  },
+  plugins: [],
+}
